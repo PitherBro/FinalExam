7e2dffa7738a01ee1f803a46c9f102d112d1b6bc
helderma <47367930+helderma@users.noreply.github.com>
2019-05-07 16:32:26 -0500

    Completed Timer
    Created a Timer in the game

---
+++ Assets/CoreComponets/Settings.cs
@@ -0,0 +1,6 @@
+﻿
+
+public static class Settings
+{
+    public static uint gameTime { get; set; }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public ScoreKeeper Score;
+    public LivesTracker Lives;
+    float timer;
+    private void Start()
+    {
+        timer = Time.deltaTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        //after 1 second...
+        if (timer >= 1f)
+        {
+            Score.IncreaseScore();
+            Lives.DecreaseLives();
+            timer = Time.deltaTime;
+        }
+    }
+}
